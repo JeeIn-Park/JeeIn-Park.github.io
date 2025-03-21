1c67f550236d21b5c0cd6e49b0d37cd443815514
JeeIn-Park <1700pji@naver.com>
2025-03-21 10:01:52 +0000

    succesful running

--- my-portfolio/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  root: "./", // Ensure root is set properly
-  publicDir: "public", // Set public directory
-});
